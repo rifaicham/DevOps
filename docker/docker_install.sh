958544a2df40d874c79a4ab29257ce1dce0ec628
Rifai cham <45489325+rifaicham@users.noreply.github.com>
2022-01-22 10:40:12 +0700

    Create docker_install.sh

---
+++ docker/docker_install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# install 
+# wget -O - https://gist.githubusercontent.com/sgnd/8ac5130ec4439985d14d118c77b7b418/raw/c351376fd4d7afbca587f8ed1f2fd57f87e113ce/docker.sh | bash
+
+# install dependencies
+# --------------------
+sudo apt install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+# add docker repository
+# ---------------------
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+
+# update operating system
+# -----------------------
+sudo apt update -y
+
+# install docker engine
+# ---------------------
+sudo apt install docker-ce docker-ce-cli containerd.io -y
+
+# run docker without sudo
+# -----------------------
+sudo usermod -aG docker $(whoami)
+
+# download docker-compose
+# -----------------------
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# change permission docker-compose
+# --------------------------------
+sudo chmod +x /usr/local/bin/docker-compose
